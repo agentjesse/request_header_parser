0a562d771648bd8306466f74399986d33fbadd88
Jesse <31576321+agentjesse@users.noreply.github.com>
2018-02-15 16:17:47 -0500

    final

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-// node/express app starts here
-
 const express = require('express')
 const app = express()
 
 app.get('/', (request, response)=> {
-  console.log('get request to /')
-  response.end()
-  // response.sendStatus(200)
+  // console.log( 'headers: ',request.headers) //everything is in here
+  response.send(
+    {
+      'IP Address': request.headers['x-forwarded-for'] ? request.headers['x-forwarded-for'].split(',')[0] : 'no x-forwarded-for header!' ,
+      'Language': request.headers['accept-language'].split(',')[0],
+      'Operating Sytem': request.headers['user-agent'].split( /[\(\)]/ )[1]
+    }
+  )
+
 })
 
 // listen for requests. uncomment line depending on server. choose first for glitch, as the .env files with the port will become available when their site hosts the app. or use the second when running this app locally
